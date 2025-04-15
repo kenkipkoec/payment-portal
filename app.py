389b5786784ab0fca030ac0212bb531c53a33eb7
Kenaki <itsmetrokenaki@gmail.com>
2025-04-15 23:15:16 +0300

    Updated login page and fixed bugs

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USERNAME'] = 'chasersbit439@gmail.com'
-app.config['MAIL_PASSWORD'] = 'opjv zgzp yafu ngcc'
+app.config['MAIL_PASSWORD'] = 'opjvzgzpyafungcc'
 app.config['MAIL_DEFAULT_SENDER'] = 'chasersbit439@gmail.com'
 
 mail = Mail(app)
